3efedbe79d07ac4b2f00182d13ea09ccb2f7395d
ReganHe <heoo547627@126.com>
2026-05-09 16:11:19 +0800

    更新 AutoAPI 配置和生成的 API 文件

--- apis/auto/demo/model.ts
+++ apis/auto/demo/model.ts
@@ -57,13 +57,6 @@ export interface UploadFileRequest {
   onUploadProgress?: (progressEvent: AxiosProgressEvent) => void;
 }
 
-/** Query user list with pagination response parameters */
-export interface GetUserPagedResponse {
-  results: Array<UserInfoDto>;
-
-  total: number;
-}
-
 export interface ResultData {
   /**
    * Status code
